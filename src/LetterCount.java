96cd4a78ac7e353209e048ecafd4a29e423ba34e
joyce <httpsabrinas99@gmail.com>
2023-02-04 18:16:11 -0500

    feat: add if and else statements

--- src/LetterCount.java
+++ src/LetterCount.java
@@ -2,27 +2,39 @@
 
 
 import java.util.Scanner;
+
 public class LetterCount {
-// displays two words and return how many letters which word has
+    // displays two words and return how many letters which word has
     // create a scanner objects
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("This program will display the number of letters in a word");
         Scanner wordsInTheName = new Scanner(System.in);
-        System.out.println("Enter the first word: " );
+        System.out.println("Enter the first word: ");
         String word = wordsInTheName.nextLine();
         System.out.println("Enter the second word: ");
         String word2 = wordsInTheName.nextLine();
+        System.out.println("The first word " + word + " has " + word.length() + " letters\nThe second word " + word2 + " has " + word2.length() + " letters ");
+        
 
-        System.out.println("The first word " + word + " has " + word.length()  + " letters\nThe second word " + word2 + " has " + word2.length() + " letters " );
+        if (word2.length() > word.length()) {
+            System.out.println(word2 + " Is bigger than " + word);
+        } else {
+            System.out.println(word + " is bigger than " + word2);
 
-       // String numberOfLetters = (wordsInTheName.nextLine());
-       // System.out.println("The numbers of letters is " + word.length());
-      //  System.out.println(numberOfLetters);
+        }
+
+    }
+}
 
 
 
-  // 2. create a variable to countNames;
 
+        // String numberOfLetters = (wordsInTheName.nextLine());
+        // System.out.println("The numbers of letters is " + word.length());
+        //  System.out.println(numberOfLetters);
+
+
+        // 2. create a variable to countNames;
 
 
         //   Scanner myObj = new Scanner(System.in);  // Create a Scanner object
@@ -32,15 +44,12 @@ public class LetterCount {
         //    System.out.println("Username is: " + userName);  // Output user input
 
 
-
-
-
-
         //
         //        System.out.printf("The surface area of a pyramid =  %.2f", surfaceOfArea);
         //
         //        double volumeOfAPyramid = (1.0 / 3.0) * length * width * height;
         //        System.out.printf(" The volume of a pyramid =  %.2f", +volumeOfAPyramid);
         //    }
-    }
-}
+
+
+
