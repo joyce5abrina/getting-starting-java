231a2283fac3dba2088ae24de052ccb34865bd2c
joyce <httpsabrinas99@gmail.com>
2023-02-04 17:14:34 -0500

    feat: add letterCount class

---
+++ src/LetterCount.java
@@ -0,0 +1,46 @@
+
+
+
+import java.util.Scanner;
+public class LetterCount {
+// displays two words and return how many letters which word has
+    // create a scanner objects
+    public static void main (String[] args) {
+        System.out.println("This program will display the number of letters in a word");
+        Scanner wordsInTheName = new Scanner(System.in);
+        System.out.println("Enter the first word: " );
+        String word = wordsInTheName.nextLine();
+        System.out.println("Enter the second word: ");
+        String word2 = wordsInTheName.nextLine();
+
+        System.out.println("The first word " + word + " has " + word.length()  + " letters\nThe second word " + word2 + " has " + word2.length() + " letters " );
+
+       // String numberOfLetters = (wordsInTheName.nextLine());
+       // System.out.println("The numbers of letters is " + word.length());
+      //  System.out.println(numberOfLetters);
+
+
+
+  // 2. create a variable to countNames;
+
+
+
+        //   Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        //    System.out.println("Enter username");
+        //
+        //    String userName = myObj.nextLine();  // Read user input
+        //    System.out.println("Username is: " + userName);  // Output user input
+
+
+
+
+
+
+        //
+        //        System.out.printf("The surface area of a pyramid =  %.2f", surfaceOfArea);
+        //
+        //        double volumeOfAPyramid = (1.0 / 3.0) * length * width * height;
+        //        System.out.printf(" The volume of a pyramid =  %.2f", +volumeOfAPyramid);
+        //    }
+    }
+}
