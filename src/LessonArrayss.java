e163debfdb2586c4c7fba91a5b852ee70c70b5e6
joyce <httpsabrinas99@gmail.com>
2023-02-11 17:19:08 -0500

    Arrays-Lesson

---
+++ src/LessonArrayss.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class LessonArrayss {
+
+    public static void main(String[] args) {
+        // Arrays
+
+        int zero = 0;
+        int one = 1;
+        int [] numbers = {2,0,1,100};
+        //int [] numbers = new int [4];
+      //  numbers [0] = 0;
+      //  numbers [1] = 1;
+    //    numbers [2] = 1;
+    //    numbers [4] = 2;
+        System.out.println(Arrays.toString(numbers));
+
+        String [] number = new String [3];
+        String [] names = {"Ali", "Maria"};
+        System.out.println(Arrays.toString(number));
+        System.out.println(number.length);
+        System.out.println(Arrays.toString(names));
+        System.out.println(names.length);
+    }
+
+
+
+
+    }
+
